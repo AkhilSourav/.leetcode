d8df04da8a5c45271c1104ce1521549dc5fc28c1
NIKHIL GAURAV <akhil.s18@iiits.in>
2022-07-01 11:29:04 +0530

    my coding files

---
+++ 2286.booking-concert-tickets-in-groups.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=2286 lang=cpp
+ *
+ * [2286] Booking Concert Tickets in Groups
+ */
+
+// @lc code=start
+class BookMyShow {
+public:
+    BookMyShow(int n, int m) {
+        
+    }
+    
+    vector<int> gather(int k, int maxRow) {
+        
+    }
+    
+    bool scatter(int k, int maxRow) {
+        
+    }
+};
+
+/**
+ * Your BookMyShow object will be instantiated and called as such:
+ * BookMyShow* obj = new BookMyShow(n, m);
+ * vector<int> param_1 = obj->gather(k,maxRow);
+ * bool param_2 = obj->scatter(k,maxRow);
+ */
+// @lc code=end
+
